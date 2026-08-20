7b6f7f7d0c0bbe3b33cceccc489af86f8d9165f9
yadavvarun1427-hash <yadavvarun1427@gmail.com>
2026-08-20 20:52:29 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='av-future-next-v1';
+const ASSETS=['./','index.html','manifest.json','icon-192.svg','icon-512.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))));
+});
